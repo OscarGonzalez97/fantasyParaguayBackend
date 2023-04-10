af6fc4356a966f896db715214838419209b1e792
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-04-10 09:38:21 -0400

    allowed host all

--- fantasyParaguayBackend/settings.py
+++ fantasyParaguayBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d0+4l2^&=!1krx!pbg(m14dv!4uuuwdug!an+xw$bjzv!s8+zd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
