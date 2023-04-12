d83bcd904cd785a0157e2b4895a7c455b6ccec54
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-04-12 09:51:30 -0400

    se agrega equipo al admin

--- fantasy/admin.py
+++ fantasy/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Jugador, Torneo
+from .models import Jugador, Torneo, Equipo
 
 admin.site.register(Jugador)
 admin.site.register(Torneo)
+admin.site.register(Equipo)
