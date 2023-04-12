e63a86550f109cd9106ba2a1fc42e6ba7777bf4a
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-04-12 02:25:05 -0400

    se termina logica principal del juego + migration

--- fantasy/models.py
+++ fantasy/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
 from auth_fantasy.models import Usuario
 
@@ -36,7 +38,7 @@ class Jugador(models.Model):
     """Jugador con toda la información necesaria para el fantasy"""
     POSICION = (
         ('DEL', 'Delantero'),
-        ('CEN', 'Mediocampista'),
+        ('MED', 'Mediocampista'),
         ('DEF', 'Defensa'),
         ('POR', 'Portero'),
     )
@@ -52,7 +54,7 @@ class Jugador(models.Model):
     nacionalidad = models.CharField('Nacionalidad', max_length=20, null=True, blank=True)
     fecha_nacimiento = models.DateField('Fecha de nacimiento', null=True, blank=True)
     equipo = models.ForeignKey(Equipo, related_name='jugadores', on_delete=models.SET_NULL, null=True, blank=True)
-    precio =models.PositiveSmallIntegerField('Precio', null=True, blank=True)
+    precio = models.PositiveSmallIntegerField('Precio', null=True, blank=True)
 
     def __str__(self):
         return self.nombre_abreviado + " " + self.posicion + " " + str(self.equipo)
@@ -62,40 +64,44 @@ class PuntuacionJugador(models.Model):
     """Puntuacion total de jugador por torneo"""
     jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE, related_name='puntuaciones')
     torneo = models.ForeignKey(Torneo, on_delete=models.CASCADE, related_name='puntuaciones_jugadores')
-    puntos = models.SmallIntegerField()
+    puntos = models.SmallIntegerField(default=0)
 
     class Meta:
         unique_together = ('jugador', 'torneo',)
 
     def __str__(self):
-        return f'Puntuación de {self.jugador} en {self.torneo}: {self.puntuacion_total}'
+        return f'Puntuación de {self.jugador} en {self.torneo}: {self.puntos}'
 
 
 class PuntuacionUsuario(models.Model):
     """Puntuacion total de usuario por torneo"""
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='puntuaciones')
     torneo = models.ForeignKey(Torneo, on_delete=models.CASCADE, related_name='puntuaciones_usuarios')
-    puntos = models.SmallIntegerField()
+    puntos = models.SmallIntegerField(default=0)
 
     class Meta:
         unique_together = ('usuario', 'torneo',)
 
     def __str__(self):
-        return f'Puntuación de {self.usuario} en {self.torneo}: {self.puntuacion_total}'
+        return f'Puntuación de {self.usuario} en {self.torneo}: {self.puntos}'
 
 
-# class EquipoUsuario(models.Model):
-#     """El equipo seleccionado(15 jugadores) por el usuario por torneo"""
-#     usuario = models.ForeignKey(Usuario, related_name='equipos', on_delete=models.CASCADE)
-#     torneo = models.ForeignKey(Torneo, related_name='equipos_usuarios', on_delete=models.CASCADE)
-#     jugadores = models.ManyToManyField(Jugador, related_name='jugadores', on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return str(self.usuario) + " " + str(self.torneo)
+class EquipoUsuario(models.Model):
+    """El equipo seleccionado(15 jugadores) por el usuario por torneo"""
+    usuario = models.ForeignKey(Usuario, related_name='equipos', on_delete=models.CASCADE)
+    torneo = models.ForeignKey(Torneo, related_name='equipos_usuarios', on_delete=models.CASCADE)
+    jugadores = models.ManyToManyField(Jugador, related_name='equipos')
+    gasto_total = models.SmallIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('usuario', 'torneo',)
+
+    def __str__(self):
+        return str(self.usuario) + " " + str(self.torneo)
 
 
 class Partido(models.Model):
-    """Un partido especifico """
+    """Un partido especifico"""
     fecha = models.DateField('Fecha', null=True, blank=True)
     timestamp = models.PositiveBigIntegerField('Epoch time', null=True, blank=True)
     timezone = models.CharField('Timezone', max_length=10, default='UTC')
@@ -112,11 +118,47 @@ class Partido(models.Model):
         return str(self.fecha) + " " + str(self.torneo) + " " + str(self.equipo_local) + "vs" + str(self.equipo_visitante)
 
 
-# class Alineacion(models.Model):
-#     """Alineacion de un partido especifico"""
-#     partido = models.ForeignKey(Partido, related_name='alineaciones', on_delete=models.CASCADE)
-#     usuario = models.OneToOneField(Usuario, related_name='usuario', on_delete=models.CASCADE)
-#     torneo = models.OneToOneField(Torneo, related_name='torneo', on_delete=models.CASCADE)
-#     jugadores = models.ManyToManyField(Jugador, related_name='jugadores', on_delete=models.CASCADE)
+class Alineacion(models.Model):
+    """Alineacion de un partido especifico"""
+    partido = models.ForeignKey(Partido, related_name='alineaciones', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, related_name='alineaciones', on_delete=models.CASCADE)
+    torneo = models.ForeignKey(Torneo, related_name='alineaciones', on_delete=models.CASCADE)
+    jugadores = models.ManyToManyField(Jugador, related_name='alineaciones', through='AlineacionJugador')
 
-# Liga: varios usuarios, puntos x cada usuario, nombre, fecha de creacion
+    class Meta:
+        unique_together = ('usuario', 'partido',)
+
+    def __str__(self):
+        return str(self.usuario) + ' ' + str(self.partido)
+
+
+class AlineacionJugador(models.Model):
+    alineacion = models.ForeignKey(Alineacion, on_delete=models.CASCADE)
+    jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE)
+    capitan = models.BooleanField(default=False)
+
+
+class Liga(models.Model):
+    """Ligas disponible para jugar"""
+    creado_por = models.ForeignKey(Usuario, related_name='ligas_creadas', null=True, blank=True,
+                                   on_delete=models.SET_NULL)
+    usuarios = models.ManyToManyField(Usuario, related_name='ligas', through='LigaUsuario')
+    nombre = models.CharField(max_length=100)
+    privado = models.BooleanField()
+    fecha_creacion = models.DateField(default=timezone.now)
+    codigo = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.codigo and self.pk:
+            self.codigo = str(uuid.uuid4())
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.creado_por) + ' ' + str(self.nombre)
+
+
+class LigaUsuario(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    liga = models.ForeignKey(Liga, on_delete=models.CASCADE)
+    puntos = models.SmallIntegerField(default=0)
+
