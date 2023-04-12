f25ea32b20edd53cf2ce1c8b3b8946f8f11e9a91
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-04-12 02:41:22 -0400

    se agregan campos utilizados en registroˆ

--- auth_fantasy/models.py
+++ auth_fantasy/models.py
@@ -3,7 +3,14 @@ from django.db import models
 
 
 class Usuario(AbstractUser):
+    GENEROS = (
+        ('hombre', 'Hombre'),
+        ('mujer', 'Mujer'),
+    )
     full_name = models.CharField('Nombre y Apellido', max_length=180, blank=True)
+    genero = models.CharField('Géneros', choices=GENEROS, max_length=50, null=True, blank=True)
+    fecha_nacimiento = models.DateField('Fecha de nacimiento', null=True, blank=True)
+    nro_telefono = models.CharField('Número de teléfono', null=True, blank=True)
 
 
     class Meta:
