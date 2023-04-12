db919d6baf421cb4d9ce1e563896cc3aeec0c05c
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-04-12 09:29:54 -0400

    se cambia de lugar rutas de auth

--- fantasy/urls.py
+++ fantasy/urls.py
@@ -3,6 +3,7 @@ from .views import TorneoList, JugadorList, PartidoList
 
 urlpatterns = [
     path('api/', include([
+        path('auth/', include('auth_fantasy.urls')),
         path('torneos/', TorneoList.as_view(), name='torneo-list'),
         path('jugadores/', JugadorList.as_view(), name='jugador-list'),
         path('partidos/', PartidoList.as_view(), name='partido-list'),
