fa107103b323dd474149e3758e4ed5bf57d4bad8
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-04-12 11:34:52 -0400

    se crea admin de partido

--- fantasy/admin.py
+++ fantasy/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Jugador, Torneo, Equipo
+from .models import Jugador, Torneo, Equipo, Partido
 
 admin.site.register(Jugador)
 admin.site.register(Torneo)
 admin.site.register(Equipo)
+admin.site.register(Partido)
