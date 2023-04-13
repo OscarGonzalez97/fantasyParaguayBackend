58ce542339dfa8a3b83b06e05d2f80cadcc0258b
Oscar Gonzalez <oscargonzalez159@gmail.com>
2023-04-13 12:29:48 -0400

    agrego admin liga

--- fantasy/admin.py
+++ fantasy/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Jugador, Torneo, Equipo, Partido
+from .models import Jugador, Torneo, Equipo, Partido, Liga
 
 admin.site.register(Jugador)
 admin.site.register(Torneo)
 admin.site.register(Equipo)
 admin.site.register(Partido)
+admin.site.register(Liga)
